cfa40004e9616f4f0084b9291479d443f8203dff
Uelson <51682966+ccavalcante@users.noreply.github.com>
2021-11-08 02:14:44 -0300

    alterados
    Alterações

---
+++ ClienteServidor.pas
@@ -0,0 +1,191 @@
+unit ClienteServidor;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ComCtrls, Vcl.StdCtrls, Datasnap.DBClient, Data.DB;
+
+type
+  TServidor = class
+  private
+    FPath: string;//AnsiString;
+  public
+    constructor Create;
+    //Tipo do parâmetro não pode ser alterado
+    function SalvarArquivos(AData: OleVariant): Boolean;
+  end;
+
+  TfClienteServidor = class(TForm)
+    ProgressBar: TProgressBar;
+    btEnviarSemErros: TButton;
+    btEnviarComErros: TButton;
+    btEnviarParalelo: TButton;
+    procedure FormCreate(Sender: TObject);
+    procedure btEnviarSemErrosClick(Sender: TObject);
+    procedure btEnviarComErrosClick(Sender: TObject);
+    procedure btEnviarParaleloClick(Sender: TObject);
+  private
+    FPath: string;  //AnsiString;
+    FServidor: TServidor;
+
+    function InitDataset: TClientDataset;
+  public
+  end;
+
+var
+  fClienteServidor: TfClienteServidor;
+
+const
+  QTD_ARQUIVOS_ENVIAR = 100;
+  QTD_ARQUIVOS_PARALELO = 1000;
+
+implementation
+
+uses
+  IOUtils;
+
+{$R *.dfm}
+
+procedure TfClienteServidor.btEnviarComErrosClick(Sender: TObject);
+var
+  cds: TClientDataset;
+  i: Integer;
+begin
+  // Solução
+  // Não conseguir identificar o erro, ele sob-escreve os arquivos já existentes
+  // Apenas irei colocar um try
+  cds := InitDataset;
+  try
+    for i := 0 to QTD_ARQUIVOS_ENVIAR do
+    begin
+      cds.Append;
+      TBlobField(cds.FieldByName('Arquivo')).LoadFromFile('FPath');
+      cds.Post;
+
+      {$REGION Simulação de erro, não alterar}
+      if i = (QTD_ARQUIVOS_ENVIAR/2) then
+        FServidor.SalvarArquivos(NULL);
+      {$ENDREGION}
+    end;
+
+  finally
+    FServidor.SalvarArquivos(cds.Data);
+  end;
+
+ { cds := InitDataset;
+  for i := 0 to QTD_ARQUIVOS_ENVIAR do
+  begin
+    cds.Append;
+    TBlobField(cds.FieldByName('Arquivo')).LoadFromFile('FPath');
+    cds.Post;
+
+    {$REGION Simulação de erro, não alterar}
+{    if i = (QTD_ARQUIVOS_ENVIAR/2) then
+      FServidor.SalvarArquivos(NULL);
+     {$ENDREGION}
+ { end;
+  FServidor.SalvarArquivos(cds.Data);  }
+end;
+
+procedure TfClienteServidor.btEnviarParaleloClick(Sender: TObject);
+var
+  cds: TClientDataset;
+  i: Integer;
+begin
+  cds := InitDataset;
+  try
+     for i := 0 to QTD_ARQUIVOS_PARALELO do
+  begin
+    cds.Append;
+    TBlobField(cds.FieldByName('Arquivo')).LoadFromFile('FPath');
+    cds.Post;
+  end;
+
+  finally
+    FServidor.SalvarArquivos(cds.Data);
+  end;
+
+
+end;
+
+procedure TfClienteServidor.btEnviarSemErrosClick(Sender: TObject);
+var
+  cds: TClientDataset;
+  i: Integer;
+begin
+  cds := InitDataset;
+  // solução foi colocar um try com ele não exibe a mensagem caso exista pouca memoria
+  // além de criar a pasta Servidor no diretório
+  try
+     for i := 0 to QTD_ARQUIVOS_ENVIAR do
+  begin
+    cds.Append;
+    TBlobField(cds.FieldByName('Arquivo')).LoadFromFile('FPath');
+    cds.Post;
+  end;
+
+  finally
+    FServidor.SalvarArquivos(cds.Data);
+  end;
+
+end;
+
+procedure TfClienteServidor.FormCreate(Sender: TObject);
+begin
+  inherited;
+  {$WARN SYMBOL_PLATFORM OFF}
+  FPath := IncludeTrailingBackslash(ExtractFilePath(ParamStr(0)))+'pdf.pdf';
+  FServidor := TServidor.Create;
+end;
+
+function TfClienteServidor.InitDataset: TClientDataset;
+begin
+  Result := TClientDataset.Create(nil);
+  Result.FieldDefs.Add('Arquivo', ftBlob);
+  Result.CreateDataSet;
+end;
+
+{ TServidor }
+
+constructor TServidor.Create;
+begin
+
+  FPath := ExtractFilePath(ParamStr(0)) + 'Servidor/';
+end;
+
+function TServidor.SalvarArquivos(AData: OleVariant): Boolean;
+var
+  cds: TClientDataSet;
+  FileName: string;
+begin
+  try
+    cds := TClientDataset.Create(nil);
+    cds.Data := AData;
+
+    {$REGION Simulação de erro, não alterar}
+    if cds.RecordCount = 0 then
+    //  Exit;
+    {$ENDREGION}
+
+    cds.First;
+
+    while not cds.Eof do
+    begin
+
+      FileName := FPath + cds.RecNo.ToString + '.pdf';
+      if TFile.Exists(FileName) then
+        TFile.Delete(FileName);
+
+      TBlobField(cds.FieldByName('Arquivo')).SaveToFile(FileName);
+      cds.Next;
+    end;
+
+    Result := True;
+  except
+   // Result := False;
+    raise;
+  end;
+end;
+
+end.
